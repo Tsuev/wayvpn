3004ae20933177692cbbb23f43f1544254cb8832
Tsuev <tsuevjudoka@gmail.com>
2024-09-16 21:53:28 +0300

    update

--- app/services/yookassaServies.js
+++ app/services/yookassaServies.js
@@ -1,3 +1,50 @@
-import yooKassa from "../plugins/yookassa";
+import fetch from "node-fetch";
+import { v4 as uuidv4 } from "uuid"; // Для генерации уникального идентификатора
+import { config } from "dotenv";
 
-function pay(amount, description, userId) {}
+config({ path: "../../.env" });
+
+const secretKey = process.env.TEST_YOOKASSA_API_KEY;
+const shopId = process.env.TEST_YOOKASSA_SHOP_ID;
+const url = process.env.YOOKASSA_URL;
+
+const createPayment = async (value) => {
+  const paymentData = {
+    amount: {
+      value,
+      currency: "RUB",
+    },
+    capture: true,
+    confirmation: {
+      type: "redirect",
+      return_url: "https://t.me/wayvpn95_bot",
+    },
+    description: "Оплата подписки",
+    metadata: {
+      order_id: uuidv4(),
+    },
+  };
+
+  const headers = {
+    "Content-Type": "application/json",
+    "Idempotence-Key": uuidv4(),
+    Authorization:
+      "Basic " + Buffer.from(`${shopId}:${secretKey}`).toString("base64"),
+  };
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(paymentData),
+    });
+
+    const responseData = await response.json();
+
+    return responseData;
+  } catch (error) {
+    console.error("Ошибка запроса:", error);
+  }
+};
+
+createPayment();
