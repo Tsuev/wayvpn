7d9486de79e9ead48f122185d46938cde4224adb
Tsuev <tsuevjudoka@gmail.com>
2024-09-27 15:51:11 +0300

    update

--- app/services/supabaseService.js
+++ app/services/supabaseService.js
@@ -38,4 +38,27 @@ const getClient = async (tg_id) => {
   }
 };
 
-export { createClient, getClient };
+const resetExpiredLeftTimeAndSubscription = async () => {
+  const currentTime = Date.now();
+  console.log("foo");
+
+  const { data, error } = await supabase
+    .from("clients")
+    .update({
+      left_time: 0,
+      subscription: false,
+    })
+    .lt("left_time", currentTime)
+    .select();
+
+  if (error) {
+    console.error("Ошибка при обновлении записей:", error);
+    return;
+  }
+
+  return data;
+};
+
+resetExpiredLeftTimeAndSubscription();
+
+export { createClient, getClient, resetExpiredLeftTimeAndSubscription };
